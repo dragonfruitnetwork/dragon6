dbb4ccc28ebcdb08d39e465c1087cdfd412c3809
Albie <albion@dragonfruit.network>
2022-10-21 10:50:30 +0100

    add file structure interface

---
+++ DragonFruit.Six.Client/Database/IFileSystemStructure.cs
@@ -0,0 +1,14 @@
+// Dragon6 Client Copyright (c) DragonFruit Network <inbox@dragonfruit.network>
+// Licensed under GNU AGPLv3. Refer to the LICENSE file for more info
+
+namespace DragonFruit.Six.Client.Database
+{
+    /// <summary>
+    /// Provides a way to set folder structures on a per-system level
+    /// </summary>
+    public interface IFileSystemStructure
+    {
+        public string Cache { get; }
+        public string AppData { get; }
+    }
+}
