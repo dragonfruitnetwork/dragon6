9e78396238e6e22d903a30fbf19bd688f4ca762b
Albie <albion@dragonfruit.network>
2022-10-25 22:13:14 +0100

    add url formatting utils

---
+++ DragonFruit.Six.Client/Utils/RankIconUtils.cs
@@ -0,0 +1,13 @@
+﻿// Dragon6 Client Copyright (c) DragonFruit Network <inbox@dragonfruit.network>
+// Licensed under GNU AGPLv3. Refer to the LICENSE file for more info
+
+namespace DragonFruit.Six.Client.Utils
+{
+    public static class RankIconUtils
+    {
+        public static string FormatRankIconUrl(string url)
+        {
+            return "https://d6static.dragonfruit.network/" + url.TrimStart('/').Replace("-png", string.Empty).Replace(".png", ".svg");
+        }
+    }
+}
