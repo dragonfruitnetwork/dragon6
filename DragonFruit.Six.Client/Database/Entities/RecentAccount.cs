39cdb0f2e304335617b871679d669357fe1436b2
Albie <albion@dragonfruit.network>
2022-10-27 21:25:00 +0100

    add recent account type

---
+++ DragonFruit.Six.Client/Database/Entities/RecentAccount.cs
@@ -0,0 +1,36 @@
+﻿// Dragon6 Client Copyright (c) DragonFruit Network <inbox@dragonfruit.network>
+// Licensed under GNU AGPLv3. Refer to the LICENSE file for more info
+
+using System;
+using DragonFruit.Six.Api.Accounts.Enums;
+using Realms;
+
+namespace DragonFruit.Six.Client.Database.Entities
+{
+    [MapTo("recent_accounts")]
+    public class RecentAccount : RealmObject
+    {
+        [PrimaryKey]
+        [MapTo("profile_id")]
+        public string ProfileId { get; set; }
+
+        [MapTo("ubisoft_id")]
+        public string UbisoftId { get; set; }
+
+        [MapTo("username")]
+        public string Username { get; set; }
+
+        [Ignored]
+        public Platform Platform
+        {
+            get => (Platform)PlatformValue;
+            set => PlatformValue = (int)value;
+        }
+
+        [MapTo("platform")]
+        private int PlatformValue { get; set; }
+
+        [MapTo("last_searched")]
+        public DateTimeOffset LastSearched { get; set; } = DateTimeOffset.Now;
+    }
+}
