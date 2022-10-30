a437040735b38dadbdc90a513b466b944a9a294b
Albie <albion@dragonfruit.network>
2022-10-30 18:27:49 +0000

    add standard button

---
+++ DragonFruit.Six.Client/Overlays/Buttons/HomeButton.cs
@@ -0,0 +1,16 @@
+﻿// Dragon6 Client Copyright (c) DragonFruit Network <inbox@dragonfruit.network>
+// Licensed under GNU AGPLv3. Refer to the LICENSE file for more info
+
+using Microsoft.AspNetCore.Components;
+
+namespace DragonFruit.Six.Client.Overlays.Buttons
+{
+    public class HomeButton : INavigationButton
+    {
+        public int Order => 0;
+        public string Icon => "home";
+        public string Name => "Home";
+
+        public void OnClick(NavigationManager nav) => nav.NavigateTo("/home");
+    }
+}
