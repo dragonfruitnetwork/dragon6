6746230feafd11f2cbc07f724c9be06e484b5987
Albie <albion@dragonfruit.network>
2022-10-30 20:36:57 +0000

    add server status indicator

---
+++ DragonFruit.Six.Client/Overlays/Status/ServerStatusButtonInfo.cs
@@ -0,0 +1,14 @@
+﻿// Dragon6 Client Copyright (c) DragonFruit Network <inbox@dragonfruit.network>
+// Licensed under GNU AGPLv3. Refer to the LICENSE file for more info
+
+using System;
+
+namespace DragonFruit.Six.Client.Overlays.Status
+{
+    public class ServerStatusButtonInfo : ICustomButton
+    {
+        public int Order => 3;
+
+        public Type ButtonComponentType => typeof(ServerStatusButton);
+    }
+}
