87148fcded15b96f296c16e72473be8648552340
Albie <albion@dragonfruit.network>
2022-11-01 21:13:22 +0000

    change navigationbuttons to servicebuttons

--- DragonFruit.Six.Client/Overlays/Buttons/HomeButton.cs
+++ DragonFruit.Six.Client/Overlays/Buttons/HomeButton.cs
@@ -1,11 +1,13 @@
 ﻿// Dragon6 Client Copyright (c) DragonFruit Network <inbox@dragonfruit.network>
 // Licensed under GNU AGPLv3. Refer to the LICENSE file for more info
 
+using System;
 using Microsoft.AspNetCore.Components;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace DragonFruit.Six.Client.Overlays.Buttons
 {
-    public class HomeButton : INavigationButton
+    public class HomeButton : IServiceButton
     {
         public int Order => 0;
         public int ColumnSize => 12;
@@ -13,6 +15,6 @@ namespace DragonFruit.Six.Client.Overlays.Buttons
         public string Icon => "home";
         public string Name => "Home";
 
-        public void OnClick(NavigationManager nav) => nav.NavigateTo("/home");
+        public void OnClick(IServiceProvider services) => services.GetRequiredService<NavigationManager>().NavigateTo("/home");
     }
 }
--- DragonFruit.Six.Client/Overlays/IServiceButton.cs
+++ DragonFruit.Six.Client/Overlays/IServiceButton.cs
@@ -1,18 +1,18 @@
 ﻿// Dragon6 Client Copyright (c) DragonFruit Network <inbox@dragonfruit.network>
 // Licensed under GNU AGPLv3. Refer to the LICENSE file for more info
 
-using Microsoft.AspNetCore.Components;
+using System;
 
 namespace DragonFruit.Six.Client.Overlays
 {
     /// <summary>
-    /// Represents a button that has a callback that requires access to the context's <see cref="NavigationManager"/>
+    /// Represents a button that has a callback that requires access to the context's <see cref="IServiceProvider"/> to perform its purpose
     /// </summary>
-    public interface INavigationButton : IHeaderButton
+    public interface IServiceButton : IHeaderButton
     {
         string Icon { get; }
         string Name { get; }
 
-        void OnClick(NavigationManager nav);
+        void OnClick(IServiceProvider services);
     }
 }
