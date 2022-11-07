7d0fe3f0844e941daabfc97d77ab9b20e2e03825
Albie <albion@dragonfruit.network>
2022-11-07 19:07:55 +0000

    add user request/response

---
+++ DragonFruit.Six.Client/Network/User/Dragon6User.cs
@@ -0,0 +1,52 @@
+﻿// Dragon6 Client Copyright (c) DragonFruit Network <inbox@dragonfruit.network>
+// Licensed under GNU AGPLv3. Refer to the LICENSE file for more info
+
+using System;
+using DragonFruit.Six.Api.Services.Verification;
+using JetBrains.Annotations;
+using Newtonsoft.Json;
+using Realms;
+
+namespace DragonFruit.Six.Client.Network.User
+{
+    [Serializable]
+    [MapTo("cached_users")]
+    [JsonObject(MemberSerialization.OptIn)]
+    public class Dragon6User : RealmObject
+    {
+        [MapTo("profile_id")]
+        [JsonProperty("profile_id")]
+        public string ProfileId { get; set; }
+
+        [Ignored]
+        public AccountRole Role
+        {
+            get => (AccountRole)RoleValue;
+            set => RoleValue = (byte)value;
+        }
+
+        [MapTo("role")]
+        [JsonProperty("role")]
+        private byte RoleValue { get; set; }
+
+        [CanBeNull]
+        [MapTo("cover")]
+        [JsonProperty("cover_img")]
+        public string CoverImg { get; set; }
+
+        [CanBeNull]
+        [MapTo("title")]
+        [JsonProperty("title")]
+        public string Title { get; set; }
+
+        [CanBeNull]
+        [MapTo("title_icon")]
+        [JsonProperty("title_icon")]
+        public string TitleIcon { get; set; }
+
+        [CanBeNull]
+        [MapTo("title_colour")]
+        [JsonProperty("title_colour")]
+        public string TitleColour { get; set; }
+    }
+}
---
+++ DragonFruit.Six.Client/Network/User/Dragon6UserRequest.cs
@@ -0,0 +1,19 @@
+﻿// Dragon6 Client Copyright (c) DragonFruit Network <inbox@dragonfruit.network>
+// Licensed under GNU AGPLv3. Refer to the LICENSE file for more info
+
+using DragonFruit.Data;
+
+namespace DragonFruit.Six.Client.Network.User
+{
+    public class Dragon6UserRequest : ApiRequest
+    {
+        public override string Path => $"https://dragon6.dragonfruit.network/api/v2/users/{ProfileId}";
+
+        public Dragon6UserRequest(string profileId)
+        {
+            ProfileId = profileId;
+        }
+
+        public string ProfileId { get; }
+    }
+}
