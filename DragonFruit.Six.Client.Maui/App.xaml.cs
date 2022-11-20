ee18adca69a99394775aa785bfc8d2972cbf5f41
Albie <albion@dragonfruit.network>
2022-11-20 19:04:16 +0000

    set window title

--- DragonFruit.Six.Client.Maui/App.xaml.cs
+++ DragonFruit.Six.Client.Maui/App.xaml.cs
@@ -1,6 +1,9 @@
 ﻿// Dragon6 Client Copyright (c) DragonFruit Network <inbox@dragonfruit.network>
 // Licensed under GNU AGPLv3. Refer to the LICENSE file for more info
 
+using Microsoft.Maui;
+using Microsoft.Maui.Controls;
+
 namespace DragonFruit.Six.Client.Maui
 {
     public partial class App
@@ -9,6 +12,14 @@ namespace DragonFruit.Six.Client.Maui
         {
             InitializeComponent();
             MainPage = new MainPage();
+        }
+
+        protected override Window CreateWindow(IActivationState activationState)
+        {
+            var window = base.CreateWindow(activationState);
+            window.Title = "Dragon6";
+
+            return window;
         }
     }
 }
