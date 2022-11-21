ad40896516561fdc3173df4345c7dfde010c6cca
Albie <albion@dragonfruit.network>
2022-11-21 20:24:45 +0000

    fix pages not loading properly due to headoutlet missing

--- DragonFruit.Six.Client.Maui/MainPage.cs
+++ DragonFruit.Six.Client.Maui/MainPage.cs
@@ -1,6 +1,7 @@
 ﻿// Dragon6 Client Copyright (c) DragonFruit Network <inbox@dragonfruit.network>
 // Licensed under GNU AGPLv3. Refer to the LICENSE file for more info
 
+using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebView.Maui;
 using Microsoft.Maui.Controls;
 
@@ -20,6 +21,11 @@ namespace DragonFruit.Six.Client.Maui
                     {
                         Selector = "#app",
                         ComponentType = typeof(Dragon6App)
+                    },
+                    new RootComponent
+                    {
+                        Selector = "head::after",
+                        ComponentType = typeof(HeadOutlet)
                     }
                 }
             };
