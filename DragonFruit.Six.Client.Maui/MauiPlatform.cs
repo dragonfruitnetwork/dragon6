a3fb493ddbe232eab86458b19a3974ec91afbbfd
Albie <albion@dragonfruit.network>
2022-11-25 19:31:06 +0000

    move windows appdata to local appdata

--- DragonFruit.Six.Client.Maui/MauiPlatform.cs
+++ DragonFruit.Six.Client.Maui/MauiPlatform.cs
@@ -1,6 +1,7 @@
 // Dragon6 Client Copyright (c) DragonFruit Network <inbox@dragonfruit.network>
 // Licensed under GNU AGPLv3. Refer to the LICENSE file for more info
 
+using System;
 using System.IO;
 using DragonFruit.Six.Client.Configuration;
 using DragonFruit.Six.Client.Database;
@@ -21,7 +22,10 @@ namespace DragonFruit.Six.Client.Maui
         }
 
         public string Cache => FileSystem.CacheDirectory;
-        public string AppData => FileSystem.AppDataDirectory;
+
+        public string AppData => CurrentPlatform == HostPlatform.Windows
+            ? Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "DragonFruit Network", "Dragon6", "Client")
+            : FileSystem.AppDataDirectory;
 
         public HostPlatform CurrentPlatform
         {
