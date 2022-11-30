d1cf6579b8c52c1f25b9d81533411074a9969039
Albie <albion@dragonfruit.network>
2022-11-30 14:16:55 +0000

    create a logging handler

---
+++ DragonFruit.Six.Client/Network/ClientLoggingHandler.cs
@@ -0,0 +1,30 @@
+﻿// Dragon6 Client Copyright (c) DragonFruit Network <inbox@dragonfruit.network>
+// Licensed under GNU AGPLv3. Refer to the LICENSE file for more info
+
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+
+namespace DragonFruit.Six.Client.Network
+{
+    public class ClientLoggingHandler : DelegatingHandler
+    {
+        private readonly ILogger _logger;
+
+        public ClientLoggingHandler(ILogger logger, HttpMessageHandler handler)
+        {
+            _logger = logger;
+            InnerHandler = handler;
+        }
+
+        protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            _logger.LogInformation("Starting HTTP request to {url}", request.RequestUri.Host);
+            var result = await base.SendAsync(request, cancellationToken).ConfigureAwait(false);
+
+            _logger.LogInformation("Request to {url} returned {status}", result.RequestMessage.RequestUri.Host, result.StatusCode);
+            return result;
+        }
+    }
+}
