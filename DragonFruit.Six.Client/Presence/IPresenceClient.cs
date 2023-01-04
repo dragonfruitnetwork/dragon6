f016eea7ac07d4e8d51d73d95592bb688ef7f1a4
Albie <albion@dragonfruit.network>
2023-01-04 11:57:05 +0000

    add basic presence abstractions

---
+++ DragonFruit.Six.Client/Presence/IPresenceClient.cs
@@ -0,0 +1,10 @@
+﻿// Dragon6 Client Copyright (c) DragonFruit Network <inbox@dragonfruit.network>
+// Licensed under GNU AGPLv3. Refer to the LICENSE file for more info
+
+namespace DragonFruit.Six.Client.Presence
+{
+    public interface IPresenceClient
+    {
+        void PushUpdate(PresenceStatus status);
+    }
+}
---
+++ DragonFruit.Six.Client/Presence/PresenceStatus.cs
@@ -0,0 +1,17 @@
+﻿// Dragon6 Client Copyright (c) DragonFruit Network <inbox@dragonfruit.network>
+// Licensed under GNU AGPLv3. Refer to the LICENSE file for more info
+
+namespace DragonFruit.Six.Client.Presence
+{
+    public class PresenceStatus
+    {
+        public PresenceStatus(string title, string subtitle)
+        {
+            Title = title;
+            Subtitle = subtitle;
+        }
+
+        public string Title { get; }
+        public string Subtitle { get; }
+    }
+}
