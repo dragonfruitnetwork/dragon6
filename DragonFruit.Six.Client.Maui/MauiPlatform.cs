86ea27d56bfb1f4433302b414f19d8184f3419cc
Albie <albion@dragonfruit.network>
2023-01-06 14:07:42 +0000

    remove unused using statement

--- DragonFruit.Six.Client.Maui/MauiPlatform.cs
+++ DragonFruit.Six.Client.Maui/MauiPlatform.cs
@@ -1,7 +1,6 @@
 // Dragon6 Client Copyright (c) DragonFruit Network <inbox@dragonfruit.network>
 // Licensed under GNU AGPLv3. Refer to the LICENSE file for more info
 
-using System;
 using System.IO;
 using DragonFruit.Six.Client.Configuration;
 using DragonFruit.Six.Client.Database;
