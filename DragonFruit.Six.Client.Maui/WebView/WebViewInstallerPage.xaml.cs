d86dc182835262acd0647db159ab5a0abed24fb6
Albie <albion@dragonfruit.network>
2023-01-08 19:27:24 +0000

    add installer page

---
+++ DragonFruit.Six.Client.Maui/WebView/WebViewInstallerPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿// Dragon6 Client Copyright (c) DragonFruit Network <inbox@dragonfruit.network>
+// Licensed under GNU AGPLv3. Refer to the LICENSE file for more info
+
+using DragonFruit.Data;
+
+namespace DragonFruit.Six.Client.Maui.WebView
+{
+    public partial class WebViewInstallerPage
+    {
+        public WebViewInstallerModel ViewModel { get; }
+
+        public WebViewInstallerPage(ApiClient client)
+        {
+            ViewModel = new WebViewInstallerModel(this, client);
+
+            InitializeComponent();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            ViewModel.PrepareForUse.Execute(null);
+        }
+    }
+}
