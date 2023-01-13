d789cbcf78e61982446be65492caf12e211f87eb
Albie <albion@dragonfruit.network>
2023-01-13 15:29:03 +0000

    add startup hook interfaces

---
+++ DragonFruit.Six.Client/Configuration/IStartupHook.cs
@@ -0,0 +1,13 @@
+﻿// Dragon6 Client Copyright (c) DragonFruit Network <inbox@dragonfruit.network>
+// Licensed under GNU AGPLv3. Refer to the LICENSE file for more info
+
+using System;
+using System.Threading.Tasks;
+
+namespace DragonFruit.Six.Client.Configuration
+{
+    public interface IStartupHook
+    {
+        public Task<StartupHookResult> ValidateOnStartup(IServiceProvider services);
+    }
+}
---
+++ DragonFruit.Six.Client/Configuration/StartupHookResult.cs
@@ -0,0 +1,20 @@
+﻿// Dragon6 Client Copyright (c) DragonFruit Network <inbox@dragonfruit.network>
+// Licensed under GNU AGPLv3. Refer to the LICENSE file for more info
+
+using System.Collections.Generic;
+using DragonFruit.Six.Client.Overlays;
+
+namespace DragonFruit.Six.Client.Configuration
+{
+    /// <summary>
+    /// Represents the result of an <see cref="IStartupHook"/> result
+    /// </summary>
+    public class StartupHookResult
+    {
+        public bool Success { get; set; }
+
+        public string Message { get; set; }
+
+        public IEnumerable<IServiceButton> Buttons { get; set; }
+    }
+}
