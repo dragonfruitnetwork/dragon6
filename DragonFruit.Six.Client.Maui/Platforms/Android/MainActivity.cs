533a94585c5fb11816c8f70be71f34f2498f63d4
Albie <albion@dragonfruit.network>
2023-01-22 18:55:41 +0000

    add intent filter to activity

--- DragonFruit.Six.Client.Maui/Platforms/Android/MainActivity.cs
+++ DragonFruit.Six.Client.Maui/Platforms/Android/MainActivity.cs
@@ -2,14 +2,14 @@
 // Licensed under GNU AGPLv3. Refer to the LICENSE file for more info
 
 using Android.App;
+using Android.Content;
 using Android.Content.PM;
 using Microsoft.Maui;
 
 namespace DragonFruit.Six.Client.Maui
 {
-    [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true,
-        ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode |
-                               ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
+    [Activity(Name = "com.dragon.six.app", Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
+    [IntentFilter(new[] { Intent.ActionView }, Categories = new[] { Intent.CategoryDefault, Intent.CategoryBrowsable }, DataScheme = "https", DataHost = "dragon6.dragonfruit.network", DataPathPrefix = "/stats/")]
     public class MainActivity : MauiAppCompatActivity
     {
     }
