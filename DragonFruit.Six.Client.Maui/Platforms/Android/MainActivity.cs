fa1a76b37212544a858c10f23f0e6686e8824a62
Albie <albion@dragonfruit.network>
2023-01-22 21:43:57 +0000

    add autoverify attribute

--- DragonFruit.Six.Client.Maui/Platforms/Android/MainActivity.cs
+++ DragonFruit.Six.Client.Maui/Platforms/Android/MainActivity.cs
@@ -9,7 +9,7 @@ using Microsoft.Maui;
 namespace DragonFruit.Six.Client.Maui
 {
     [Activity(Name = "com.dragon.six.app", Theme = "@style/Maui.SplashTheme", MainLauncher = true, LaunchMode = LaunchMode.SingleTask, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
-    [IntentFilter(new[] { Intent.ActionView }, Categories = new[] { Intent.CategoryDefault, Intent.CategoryBrowsable }, DataSchemes = new[] { "http", "https" }, DataHost = "dragon6.dragonfruit.network", DataPathPrefix = "/stats/")]
+    [IntentFilter(new[] { Intent.ActionView }, Categories = new[] { Intent.CategoryDefault, Intent.CategoryBrowsable }, DataSchemes = new[] { "http", "https" }, DataHost = "dragon6.dragonfruit.network", DataPathPrefix = "/stats/", AutoVerify = true)]
     public class MainActivity : MauiAppCompatActivity
     {
     }
