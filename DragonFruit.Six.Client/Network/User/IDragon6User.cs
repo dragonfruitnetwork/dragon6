426795bc11fb28d4749b1fc3761121b4e7761f8d
Albie <albion@dragonfruit.network>
2023-01-30 20:31:42 +0000

    create user interfaces

---
+++ DragonFruit.Six.Client/Network/User/IDragon6User.cs
@@ -0,0 +1,19 @@
+﻿// Dragon6 Client Copyright (c) DragonFruit Network <inbox@dragonfruit.network>
+// Licensed under GNU AGPLv3. Refer to the LICENSE file for more info
+
+using DragonFruit.Six.Client.Database.Entities;
+
+namespace DragonFruit.Six.Client.Network.User
+{
+    public interface IDragon6User
+    {
+        string UbisoftId { get; }
+        AccountRole AccountRole { get; }
+
+        IDragon6UserCoverSources Covers { get; }
+
+        string Title { get; }
+        string TitleIcon { get; }
+        string TitleColour { get; }
+    }
+}
