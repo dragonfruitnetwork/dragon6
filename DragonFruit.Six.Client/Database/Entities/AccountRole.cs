c9365eb0d63b73bc6e79367e02a992208ec154f3
Albie <albion@dragonfruit.network>
2023-01-30 20:32:11 +0000

    port ex-api classes

---
+++ DragonFruit.Six.Client/Database/Entities/AccountRole.cs
@@ -0,0 +1,20 @@
+﻿// Dragon6 Client Copyright (c) DragonFruit Network <inbox@dragonfruit.network>
+// Licensed under GNU AGPLv3. Refer to the LICENSE file for more info
+
+namespace DragonFruit.Six.Client.Database.Entities
+{
+    public enum AccountRole
+    {
+        BlockedByAdmin = -2,
+        BlockedBySelf = -1,
+
+        Normal = 0,
+
+        Beta = 1,
+        Translator = 2,
+        Verified = 3,
+        Supporter = 4,
+        Contributor = 5,
+        Developer = 6
+    }
+}
