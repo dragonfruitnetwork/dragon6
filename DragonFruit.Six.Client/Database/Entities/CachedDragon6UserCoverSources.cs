19642e452398bbc4bea96d94fa527431a4b26238
Albie <albion@dragonfruit.network>
2023-01-30 20:32:01 +0000

    add user objects and realm versions

---
+++ DragonFruit.Six.Client/Database/Entities/CachedDragon6UserCoverSources.cs
@@ -0,0 +1,23 @@
+﻿// Dragon6 Client Copyright (c) DragonFruit Network <inbox@dragonfruit.network>
+// Licensed under GNU AGPLv3. Refer to the LICENSE file for more info
+
+using DragonFruit.Six.Client.Network.User;
+using Realms;
+
+namespace DragonFruit.Six.Client.Database.Entities
+{
+    public class CachedDragon6UserCoverSources : RealmObject, IDragon6UserCoverSources
+    {
+        [MapTo("raw")]
+        public string Raw { get; set; }
+
+        [MapTo("large")]
+        public string Large { get; set; }
+
+        [MapTo("mobile")]
+        public string Mobile { get; set; }
+
+        [MapTo("banner")]
+        public string Banner { get; set; }
+    }
+}
---
+++ DragonFruit.Six.Client/Network/User/Dragon6UserCovers.cs
@@ -0,0 +1,22 @@
+﻿// Dragon6 Client Copyright (c) DragonFruit Network <inbox@dragonfruit.network>
+// Licensed under GNU AGPLv3. Refer to the LICENSE file for more info
+
+using Newtonsoft.Json;
+
+namespace DragonFruit.Six.Client.Network.User
+{
+    public class Dragon6UserCovers : IDragon6UserCoverSources
+    {
+        [JsonProperty("raw")]
+        public string Raw { get; set; }
+
+        [JsonProperty("large")]
+        public string Large { get; set; }
+
+        [JsonProperty("mobile")]
+        public string Mobile { get; set; }
+
+        [JsonProperty("banner")]
+        public string Banner { get; set; }
+    }
+}
