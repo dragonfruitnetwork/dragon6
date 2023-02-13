4ed2ad25e246486d2c31d6ea1696a0e6ff81f23a
Albie <albion@dragonfruit.network>
2023-02-13 17:15:33 +0000

    migrate realm to use string names over enums

--- DragonFruit.Six.Client/Database/RealmConfigurator.cs
+++ DragonFruit.Six.Client/Database/RealmConfigurator.cs
@@ -2,6 +2,7 @@
 // Licensed under GNU AGPLv3. Refer to the LICENSE file for more info
 
 using System.IO;
+using DragonFruit.Six.Api.Accounts.Enums;
 using DragonFruit.Six.Client.Database.Entities;
 using Realms;
 
@@ -9,7 +10,7 @@ namespace DragonFruit.Six.Client.Database
 {
     public static class RealmConfigurator
     {
-        private const int SchemaVersion = 3;
+        private const int SchemaVersion = 4;
         private const string RealmName = "dragon6.realm";
 
         public static void Initialise(IDragon6Platform devicePlatform)
@@ -35,6 +36,21 @@ namespace DragonFruit.Six.Client.Database
 
                 case 2: // change cached ubisoft account to store platform name over enum
                     migration.NewRealm.RemoveAll<CachedUbisoftAccount>();
+                    break;
+
+                case 3: // change saved/recent profiles to use string-based platform names
+                    foreach (var account in migration.NewRealm.All<SavedAccount>())
+                    {
+                        var oldAccount = migration.OldRealm.DynamicApi.Find("saved_accounts", account.ProfileId);
+                        account.PlatformName = PlatformUtils.ToPlatformName((Platform)oldAccount.platform);
+                    }
+
+                    foreach (var recent in migration.NewRealm.All<RecentAccount>())
+                    {
+                        var oldRecent = migration.OldRealm.DynamicApi.Find("recent_accounts", recent.ProfileId);
+                        recent.PlatformName = PlatformUtils.ToPlatformName((Platform)oldRecent.platform);
+                    }
+
                     break;
             }
         }
